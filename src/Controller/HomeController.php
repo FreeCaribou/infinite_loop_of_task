ecb327c8edcb010fc201e5b7616a2103a75b76ea
FreeCaribou <kingdomflo16@gmail.com>
2025-06-25 15:43:46 +0200

    Make delete and 'is done' for a task

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,11 +10,12 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Form\TaskType;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Task;
+use DateTime;
 
 class HomeController extends AbstractController
 {
 
-    #[Route('/', name: 'home', methods: ['GET', 'POST', 'DELETE', 'PATCH'])]
+    #[Route('/', name: 'home', methods: ['GET', 'POST'])]
     public function home(TaskRepository $repo, Request $request, EntityManagerInterface $entityManager): Response
     {
         $newTask = new Task();
@@ -36,6 +37,26 @@ class HomeController extends AbstractController
             'tasks' => $tasks,
             'form' => $form,
         ]);
+    }
+
+    #[Route('/task-delete/{id}', name: 'task-delete')]
+    public function taskDelete($id, TaskRepository $repo, EntityManagerInterface $entityManager): Response
+    {
+        $taskToDelete = $repo->find($id);
+        $entityManager->remove($taskToDelete);
+        $entityManager->flush();
+        $this->addFlash('success', 'La tache a bien été supprimée !');
+        return $this->redirectToRoute('home');
+    }
+
+    #[Route('/task-renew/{id}', name: 'task-renew')]
+    public function taskRenew($id, TaskRepository $repo, EntityManagerInterface $entityManager): Response
+    {
+        $taskToRenew = $repo->find($id);
+        $taskToRenew->setLastDone(new DateTime());
+        $entityManager->flush();
+        $this->addFlash('success', 'Bravo d\'avoir fait la tache !');
+        return $this->redirectToRoute('home');
     }
 
 }
